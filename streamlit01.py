35080286b4df725f7549b4bbc2f6cd11dc262fc4
burferr <13796611982@163.com>
2024-06-05 22:25:31 +0800

    Add files via upload

---
+++ streamlit01.py
@@ -0,0 +1,47 @@
+import streamlit as st
+st.title("Hello,2208!")
+st.header("You need to learn some basic operations")
+st.subheader("eg,Visual StudioCode")
+st.text("It‘s not that hard")
+st.markdown("this is an image: \n \
+            ![](https://www.tjnu.edu.cn/__local/2/AE/41/C1274DD9685B10B9F814C69BEDB_84ADD00D_88D66.png)")
+
+if st.checkbox("show/Hide"):
+    st.text("You checked the box")
+status =st.radio("select gender:",
+                 ('Male','Female'))
+
+if status=='Male':
+    st.success("Male")
+else:
+    st.success("FeMale")
+
+hobbies=st.multiselect("Hobbies:",
+                       ['Reading',
+                        'Writing',
+                        'Coding',
+                        'Traveling'])
+st.write("You selected:",hobbies)
+
+if st.button("about"):
+    st.text("Streamlit is a great tool")
+    
+name =st.text_input("Enter yout name:")
+if st.button("Submit"):
+    st.write("Hello,",name)
+
+level =st.slider("select your level",1,5)
+st.write("You selected:",level)
+    
+from fastai.vision.all import *
+upload_img=st.file_uploader("Upload an image",
+                            type=['jpg',
+                                  'png'])
+if upload_img is not None:
+    img=PILImage.create(upload_img)
+    st.image(img.to_thumb(256,256),
+             caption="uoloaded Image")
+
+
+
+                 
